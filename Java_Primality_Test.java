9bc9e0104b4b10f922fed73b3a6fc1799268ac7f
Bahadır Sina Terzioğlu <bahadir.terzioglu@stu.fsm.edu.tr>
2025-07-30 20:57:59 +0300

    Add Java Primality Test

---
+++ Java_Primality_Test.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.math.*;
+public class Java_Primality_Test {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+        
+        bufferedReader.close();
+        
+        BigInteger number = new BigInteger(n);
+
+        if (number.isProbablePrime(20)) {
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
+        }
+    }
+}
