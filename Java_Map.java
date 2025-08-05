42fd592822c0a5b59da5f3de45a1e0e27e36b270
Bahadır Sina Terzioğlu <bahadir.terzioglu@stu.fsm.edu.tr>
2025-08-05 22:08:17 +0300

    Add Java Map

---
+++ Java_Map.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Java_Map {
+	public static void main(String []argh)
+	{
+		Scanner in = new Scanner(System.in);
+        Map<String, Integer> numaralar = new HashMap<>();
+		int n=in.nextInt();
+		in.nextLine();
+		for(int i=0;i<n;i++)
+		{
+			String name=in.nextLine();
+			int phone=in.nextInt();
+            numaralar.put(name, phone);
+			in.nextLine();
+		}
+		while(in.hasNext())
+		{
+			String s=in.nextLine();
+            if(numaralar.containsKey(s) == true){
+                System.out.println(s + "=" + numaralar.get(s));
+            } else{
+                System.out.println("Not found");
+            }
+		}
+	}
+}
