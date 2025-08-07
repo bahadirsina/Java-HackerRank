f1cfc38ff2970dc77fa72e3bcaa8e0b0ff44aa2e
Bahadır Sina Terzioğlu <bahadir.terzioglu@stu.fsm.edu.tr>
2025-08-07 21:57:42 +0300

    Add Java Hashset

---
+++ Java_Hashset.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Java_Hashset {
+	 public static void main(String[] args) {
+	        Scanner s = new Scanner(System.in);
+	        int t = s.nextInt();
+	        String [] pair_left = new String[t];
+	        String [] pair_right = new String[t];
+	        
+	        for (int i = 0; i < t; i++) {
+	            pair_left[i] = s.next();
+	            pair_right[i] = s.next();
+	        }
+	        HashSet<String> pairs = new HashSet<>();
+	        
+	        for(int i = 0 ; i < t; i++){
+	            String str = pair_left[i] + " " + pair_right[i];
+	            String rev = pair_right[i] + " " + pair_left[i];
+	            if(!pairs.contains(rev)){
+	                pairs.add(str);
+	            }
+	            System.out.println(pairs.size());
+	        }
+	    }
+	}
+
